e158cc6496cf3cc0346cdcd6a7943b16c6957221
Colby Newman <colby.newman2000@gmail.com>
2016-09-08 12:24:31 -0400

    Command ENUMs

---
+++ RPiTiLcd/T6A04ACommands.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPiTiLcd
+{
+    [Flags]
+    public enum T6A04ACommands
+    {
+        WordLengthSet = 0,
+        DisplayStateSet = 2,
+        CounterModeSet = 4,
+        OpAmpControl2Set = 8,
+        OpAmpControl1Set = 16,
+        YAddressSet = 32,
+        ZAddressSet = 64,
+        XAddressSet = 128,
+        ContrastSet = 192
+    }
+
+    [Flags]
+    public enum DisplayState
+    {
+        Off = 0,
+        On = 1
+    }
+
+    [Flags]
+    public enum WordLength
+    {
+        SixBits = 0,
+        EightBits = 1
+    }
+
+    [Flags]
+    public enum CounterMode
+    {
+        XDown = 0,
+        XUp = 1,
+        YDown = 2,
+        YUp = 3
+    }
+}
