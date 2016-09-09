36d2a34d287533c1c9a14208346a207cbfbaf12f
Colby Newman <colby.newman2000@gmail.com>
2016-09-09 08:45:15 -0400

    font start

---
+++ RPiTiLcd/TiCharacter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPiTiLcd
+{
+    class TiCharacter
+    {
+        public char Character;
+        public bool[,] Pixels;
+
+        public TiCharacter(char c, bool[,] pixels)
+        {
+            Character = c;
+            Pixels = pixels;
+        }
+    }
+}
